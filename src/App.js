06eb620f6e7d5552d1a7b54033883a4963e20b41
Tom Knight <tomnite@gmail.com>
2018-07-19 17:56:20 -0400

    updated filenames

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import Header from "./components/header";
-import Card from "./components/card";
-import Wrapper from "./components/wrapper";
+import Header from "./components/Header";
+import Card from "./components/Card";
+import Wrapper from "./components/Wrapper";
 import cards from "./cards.json";
 
 class App extends Component {
